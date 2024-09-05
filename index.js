c6c0d96e75050fc8f4a984f8ed29e160df97be4b
Brunao544 <bruneuvisk525@outlook.com>
2024-09-05 09:30:54 -0300

    Auto Connect Fivem Bot

---
+++ index.js
@@ -0,0 +1,33 @@
+require('dotenv').config()
+const { GatewayIntentBits } = require("discord.js")
+const Client = require('./src/structures/Client');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildIntegrations,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildWebhooks,
+        GatewayIntentBits.GuildInvites,
+        GatewayIntentBits.GuildVoiceStates,
+        GatewayIntentBits.GuildPresences,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMessageReactions,
+        GatewayIntentBits.GuildMessageTyping,
+        GatewayIntentBits.DirectMessages,
+        GatewayIntentBits.DirectMessageReactions,
+        GatewayIntentBits.DirectMessageTyping,
+        GatewayIntentBits.AutoModerationConfiguration,
+        GatewayIntentBits.AutoModerationExecution,
+        GatewayIntentBits.DirectMessagePolls,
+        GatewayIntentBits.DirectMessageReactions,
+        GatewayIntentBits.DirectMessageTyping,
+        GatewayIntentBits.DirectMessages,
+        GatewayIntentBits.GuildMessagePolls,
+        GatewayIntentBits.GuildModeration,
+        GatewayIntentBits.GuildScheduledEvents
+    ]
+});
+
+client.login(process.env.BOT_TOKEN)
